bd5aa426e75a11557f046d3d9c6d2ee12eab6cf7
gene222-arch <genephillip222@gmail.com>
2021-09-13 02:41:54 +0800

    style: enhance Forbidden page UI

--- src/views/pages/errors/Forbidden.jsx
+++ src/views/pages/errors/Forbidden.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { Card, Container, CardContent, Grid, Typography, Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const forbiddenUseStyles = makeStyles(theme => ({
+    container: {
+        height: '100vh'
+    }
+}));
 
 const Forbidden = () => 
 {
+    const classes = forbiddenUseStyles();
+    const history = useHistory();
+
     return (
-        <>
-            Forbidden Page
-        </>
+        <Container maxWidth="lg">
+            <Grid container spacing={1} alignItems='center' justify='center' className={ classes.container }>
+                <Grid item xs={ 12 } sm={ 12 } md={ 6 } lg={ 6 }>
+                    <Typography variant="h4" color="initial" align='center' gutterBottom>YOU SHALL NOT PASS</Typography>
+                    <Typography 
+                        variant="h1" 
+                        color='error' 
+                        align='center' 
+                        gutterBottom
+                    >
+                        <strong>403</strong>
+                    </Typography>
+                    <Typography variant="h5" color="initial" align='center' gutterBottom>
+                        We are sorry, but you do not have access to this page or resource.
+                    </Typography>
+                    <Button 
+                        variant="contained" 
+                        color="default" 
+                        onClick={ () => history.goBack() }
+                        fullWidth
+                    >
+                        Go Back
+                    </Button>
+                </Grid>
+                <Grid item xs={ 12 } sm={ 12 } md={ 6 } lg={ 6 }>
+                    
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
