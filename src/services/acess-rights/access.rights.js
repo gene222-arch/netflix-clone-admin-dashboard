76ae6d8321cae249e13fab8e516865944004832b
gene222-arch <genephillip222@gmail.com>
2021-09-13 02:57:46 +0800

    feat: create access rights api methods

---
+++ src/services/acess-rights/access.rights.js
@@ -0,0 +1,41 @@
+import axiosInstance from '../../utils/axiosInstance'
+
+export const fetchAllAsync = async () => 
+{
+    return await axiosInstance()
+        .get('/access-rights')
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const findByIDAsync = async (id) => 
+{
+    return await axiosInstance()
+        .get(`/access-rights/${ id }`)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const createAsync = async (payload) => 
+{
+    return await axiosInstance()
+        .post(`/access-rights`, payload)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const updateAsync = async (payload) => 
+{
+    return await axiosInstance()
+        .put(`/access-rights/${ payload.id }`, payload)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const deleteAsync = async (ids) => 
+{
+    return await axiosInstance()
+        .delete(`/access-rights`, {data: { ids }})
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
