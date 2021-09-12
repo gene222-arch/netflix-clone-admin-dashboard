a306ef520ba6b47d3fe71d02fd5bd982eae090a7
gene222-arch <genephillip222@gmail.com>
2021-09-13 01:07:59 +0800

    feat: update login api request

--- src/services/auth/login.js
+++ src/services/auth/login.js
@@ -3,7 +3,7 @@ import axiosInstance from '../../utils/axiosInstance'
 export const loginAsync = async (payload) => 
 {
     return await axiosInstance()
-        .post('/auth/login?withRoles=1', payload)
+        .post('/auth/login?withRoles=1&withPermissions=1', payload)
         .then(response => response.data)
         .catch(error => Promise.reject(error.response.data));
 }
