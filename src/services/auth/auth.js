e6e7ca820e57597c2c18e71eada334345659d5b9
gene222-arch <genephillip222@gmail.com>
2021-09-15 08:40:14 +0800

    feat: create checkPasswordAsync api request function

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -7,3 +7,11 @@ export const fetchAuthAsync = async () =>
         .then(response => response.data)
         .catch(error => Promise.reject(error.response.data));
 }
+
+export const checkPasswordMatchAsync = async (payload) => 
+{
+    return await axiosInstance()
+        .post('/auth/check-password', payload)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
