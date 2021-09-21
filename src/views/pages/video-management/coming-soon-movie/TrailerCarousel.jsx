3b948cd149a1a1ca858494bee84745322dabafdd
gene222-arch <genephillip222@gmail.com>
2021-09-22 03:46:27 +0800

    feat: added delete confirmation dialog

--- src/views/pages/video-management/coming-soon-movie/TrailerCarousel.jsx
+++ src/views/pages/video-management/coming-soon-movie/TrailerCarousel.jsx
@@ -4,12 +4,31 @@ import EditIcon from '@material-ui/icons/Edit';
 import { Typography, IconButton, AppBar, Toolbar } from '@material-ui/core';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 import trailerCarouselUseStyles from './../../../../assets/js/material-ui/trailerCarouselUseStyles';
+import ConfirmationDialog from './../../../../components/ConfirmationDialog';
+import * as CONFIRM_ACTION from './../../../../redux/modules/confirm/actions'
+import { useDispatch, connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectComingSoonMovie } from '../../../../redux/modules/coming-soon-movie/selector';
 
-const TrailerCarousel = ({ id, trailers, handleClickEdit, handleClickDelete, ...props }) =>
+const TrailerCarousel = ({ COMING_SOON_MOVIE, id, trailers, handleClickEdit, handleClickDelete, ...props }) =>
 {
     const classes = trailerCarouselUseStyles();
+    const dispatch = useDispatch();
     
     const [ carouselIndex, setCarouselIndex ] = useState(0);
+
+    const toggleShowDeleteConfirmation = () => {
+        dispatch(CONFIRM_ACTION.showConfirmationDialog({
+            mainHeader: 'Delete selected item',
+            subHeader: 'Once deleted, recovery of data is not possible',
+            confirmCallback: handleClickConfirm
+        }));
+    }
+    const handleClickConfirm = () => 
+    {
+        handleClickDelete(carouselIndex);
+        toggleShowDeleteConfirmation();
+    }
 
     useEffect(() => {
         return () => {
@@ -19,15 +38,28 @@ const TrailerCarousel = ({ id, trailers, handleClickEdit, handleClickDelete, ...
 
     return (
         <>
+            <ConfirmationDialog />
             {
                 trailers.length > 0 && (
                     <AppBar position='static'>
                         <Toolbar>
                             <Typography variant='h6' className={classes.title}>{ trailers[carouselIndex].title }</Typography>
-                            <IconButton edge='start' color='inherit' aria-label='edit' onClick={ () => handleClickDelete(carouselIndex) } >
+                            <IconButton 
+                                edge='start' 
+                                color='inherit' 
+                                aria-label='edit' 
+                                onClick={ toggleShowDeleteConfirmation } 
+                                disabled={ COMING_SOON_MOVIE.isLoading }
+                            >
                                 <DeleteOutlinedIcon />
                             </IconButton>
-                            <IconButton edge='start' color='inherit' aria-label='delete' onClick={ () => handleClickEdit(carouselIndex) } >
+                            <IconButton 
+                                edge='start' 
+                                color='inherit' 
+                                aria-label='delete' 
+                                onClick={ () => handleClickEdit(carouselIndex) } 
+                                disabled={ COMING_SOON_MOVIE.isLoading }
+                            >
                                 <EditIcon />
                             </IconButton>
                         </Toolbar>
@@ -36,22 +68,19 @@ const TrailerCarousel = ({ id, trailers, handleClickEdit, handleClickDelete, ...
             }  
             <Carousel onChange={ index => setCarouselIndex(index) }>
                 {
-                    trailers.map(({ video_path }, i) => <Item key={ i } path={ video_path } />)
+                    trailers.map(({ video_path }, i) => (
+                        <video className={ classes.video } controls>
+                            <source src={ video_path } type='video/mp4'/>
+                        </video>
+                    ))
                 }
             </Carousel>
         </>
     )
 }
 
-const Item = ({ path }) =>
-{
-    const classes = trailerCarouselUseStyles();
-    
-    return (
-        <video className={ classes.video } controls >
-            <source src={ path } type='video/mp4'/>
-        </video>
-    )
-}
+const mapStateToProps = createStructuredSelector({
+    COMING_SOON_MOVIE: selectComingSoonMovie
+});
 
-export default TrailerCarousel
+export default connect(mapStateToProps)(TrailerCarousel)
