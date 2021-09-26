83f2c4f493e7832d8b8b1022061dedc777b8c9c9
gene222-arch <genephillip222@gmail.com>
2021-09-26 11:50:44 +0800

    style: update not found message

--- src/components/not-found-components/DataNotFound.jsx
+++ src/components/not-found-components/DataNotFound.jsx
@@ -3,12 +3,22 @@ import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core';
+import Colors from './../../constants/Colors';
 
 const dataNotFoundUseStyles = makeStyles(theme => ({
     icon: {
         fontSize: '10rem',
         width: '100%',
         textAlign: 'center'
+    },
+    notFoundText: {
+        backgroundColor: theme.palette.warning.main,
+        color: Colors.darkMode,
+        padding: '1rem',
+        borderRadius: 5
+    },
+    mark: {
+        color: theme.palette.error.main
     }
 }));
 
@@ -17,24 +27,31 @@ const DataNotFound = ({ type, Icon }) =>
     const classes = dataNotFoundUseStyles();
 
     return (
-        <Container maxWidth="sm">
-            <Grid container spacing={1}>
+        <Container maxWidth="md">
+            <Grid container spacing={2}>
                 <Grid item xs={ 12 }>
                     <Icon className={ classes.icon } />
                 </Grid>
                 <Grid item xs={ 12 }>
                     <Grid container spacing={1} justify='center' alignItems='center'>
-                        <Grid item>
-                            <Typography variant="h3" color="initial" align='center'>{ type } Is</Typography>
+                        <Grid item xs={ 12 }>
+                            <Typography variant="h3" color="initial" align='center'>{ type.toUpperCase() }</Typography>
                         </Grid>
-                        <Grid item>
-                            <Typography variant="h3" color='error'>Not Found</Typography>
+                        <Grid item xs={ 12 }>
+                            <Typography
+                                variant="h1"
+                                align='center'
+                                className={ classes.notFoundText }
+                            >
+                                NOT FOUND
+                                <span className={ classes.mark }>!</span>
+                            </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
                 <Grid item xs={ 12 }>
                     <Typography variant="subtitle1" color="textSecondary" align='center'>
-                        Please try again.
+                        The { type } that you are finding may have been already deleted or yet to be created.
                     </Typography>
                 </Grid>
             </Grid>
