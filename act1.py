b6dd10df3ff5c73a9e380abd33892c0073b3bb33
Gene Phillip D. Artista <57708075+gene222-arch@users.noreply.github.com>
2021-10-03 20:10:10 +0800

    delete: act1.py

--- act1.py
+++
@@ -1,8 +0,0 @@
-print("How many courses enrolled?")
-courseGrade = ["CC 101 grade: ", "CC 102 grade: ", "MS 101 grade: ", "GE 101 grade: ", "GE 102 grade: ", "PE 101 grade: ", "RS 101 grade: "]
-  
-i = 0
-while i <= 6:
-  print(courseGrade[i])
-  grade = input()
-  i += 1
