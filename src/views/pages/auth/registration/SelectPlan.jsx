8e301dc4b0c7948c88d1d5cbc42fe753703e37f4
gene222-arch <genephillip222@gmail.com>
2021-10-30 13:18:04 +0800

    feat: separate component

--- src/views/pages/auth/registration/SelectPlan.jsx
+++ src/views/pages/auth/registration/SelectPlan.jsx
@@ -1,39 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Container from '@material-ui/core/Container'
-import AuthLayoutHeader from '../../../../components/app/AuthLayoutHeader'
 import { useHistory, useLocation } from 'react-router-dom';
-import { Card, CardContent, CardHeader, Grid } from '@material-ui/core';
-import { makeStyles, Typography, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/Check';
 import Colors from '../../../../constants/Colors';
-import { useDispatch, connect } from 'react-redux';
 import PATH from './../../../../routes/path';
-
-const PLAN_TYPES = [
-    {
-        type: 'Basic',
-        cost: 'P 100.00',
-        servicesOffered: [
-            '1 (One) month',
-        ]
-    },
-    {
-        type: 'Standard',
-        cost: 'P 200.00',
-        servicesOffered: [
-            '2 (Two) months',
-            ''
-        ]
-    },
-    {
-        type: 'Premium',
-        cost: 'P 600.00',
-        servicesOffered: [
-            '6 (Six) months',
-            ''
-        ]
-    },
-];
+import PlanTypeList from './PlanTypeList';
 
 const MESSAGES = [
     'Watch all you want',
@@ -42,45 +15,20 @@ const MESSAGES = [
 ];
 
 const selectPlanUseStyles = makeStyles(theme => ({
-    card: {
-        marginBottom: theme.spacing(2),
-        height: '26.8vh',
-        cursor: 'pointer'
-    },
-    cardContainer: {
-
-    },
-    continueButton: {
-        width: '100%',
-        height: '2.65rem'
-    },
-    selectedCardContainer: {
-        boxShadow: '0 0 2px 2px rgba(255, 255, 255, 1)'
-    },
-    unSelectedCardContainer: {
-        opacity: 0.6
-    },
-    selectedPlanTypeText: {
-        color: Colors.netflixRed,
-        fontWeight: 'bold'
-    },
     checkIcon: {
         color: Colors.netflixRed
-    },
-    planListContainer: {
-        marginTop: theme.spacing(5)
     }
 }));
 
-const SelectPlan = ({ AUTH }) => 
+const SelectPlan = () => 
 {
     const classes = selectPlanUseStyles();
-    const dispatch = useDispatch();
     const history = useHistory();
     const { state } = useLocation();
 
     const [ cardIndex, setCardIndex ] = useState(0);
     const [ planType, setPlanType ] = useState('Basic');
+    const [ isPlanTypeSet, setIsPlanTypeSet ] = useState(false);
 
     const handleClickContinue = () => {
         history.push(PATH.REGISTER, {
@@ -97,6 +45,7 @@ const SelectPlan = ({ AUTH }) =>
     useEffect(() => {
         return () => {
             setPlanType('Basic');
+            setIsPlanTypeSet(false);
         }
     }, []);
 
@@ -120,68 +69,11 @@ const SelectPlan = ({ AUTH }) =>
                             ))
                         }
                     </Grid>
-                    <Grid item xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
-                        <Grid 
-                            container 
-                            justify='space-evenly' 
-                            className={ classes.planListContainer }
-                        >
-                        {
-                            PLAN_TYPES.map(({ type, cost, servicesOffered }, index) => (
-                                <Grid 
-                                    item
-                                    key={ index } 
-                                    xs={ 12 } sm={ 6 } md={ 5 } lg={ 5 } 
-                                    className={ classes.cardContainer }
-                                    onClick={ () => handleClickCardPlan(index, type) }
-                                >
-                                    <Card 
-                                        className={ 
-                                                `${ classes.card } 
-                                                ${ cardIndex === index 
-                                                    ? classes.selectedCardContainer 
-                                                    : classes.unSelectedCardContainer 
-                                                }` 
-                                        }
-                                    >
-                                        <CardHeader
-                                            title={ 
-                                                <> 
-                                                    <Typography 
-                                                        variant="h6" 
-                                                        color="textSecondary" 
-                                                        align='center' 
-                                                        gutterBottom
-                                                        className={ cardIndex === index ? classes.selectedPlanTypeText : '' }
-                                                    >
-                                                        { type }
-                                                    </Typography>
-                                                    <Typography variant="h5" color="initial" align='center'><strong>{ cost }</strong></Typography>
-                                                </> 
-                                            }
-                                        />
-                                        <CardContent>
-                                            {
-                                                servicesOffered.map((service, index) => (
-                                                    <Typography key={ index } variant="subtitle1" color="textSecondary" align='center'>{ service }</Typography>
-                                                ))
-                                            }
-                                        </CardContent>
-                                        <Button 
-                                                variant='contained' 
-                                                color='default' 
-                                                className={ classes.continueButton }
-                                                disabled={ index !== cardIndex }
-                                                onClick={ handleClickContinue }
-                                            >
-                                                Get Started
-                                            </Button>
-                                    </Card>
-                                </Grid>
-                            ))
-                        }
-                        </Grid>
-                    </Grid>
+                    <PlanTypeList 
+                        cardIndex={ cardIndex }
+                        handleClickCardPlan={ handleClickCardPlan } 
+                        handleClickContinue={ handleClickContinue } 
+                    />
                 </Grid>
             </Container>
         </Container>
