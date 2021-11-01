277318d64b4bb3af582ffe12529764cbe61dc120
gene222-arch <genephillip222@gmail.com>
2021-11-01 08:37:01 +0800

    feat: add broadcast listener

--- src/views/layouts/UserLayout.jsx
+++ src/views/layouts/UserLayout.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import AppHeader from '../../components/app/AppHeader'
 import { makeStyles } from '@material-ui/core/styles'
 import Colors from './../../constants/Colors';
+import ECHO_UTIL from './../../utils/echo'
+import { createStructuredSelector } from 'reselect';
+import { selectAuth } from './../../redux/modules/auth/selector';
+import { connect } from 'react-redux';
 
 const userLayoutUseStyles = makeStyles(theme => ({
     container: {
@@ -12,9 +16,24 @@ const userLayoutUseStyles = makeStyles(theme => ({
     }
 }));
 
-const UserLayout = ({ children }) => 
+const UserLayout = ({ AUTH, children }) => 
 {
     const classes = userLayoutUseStyles();
+
+    useEffect(() => {
+        ECHO_UTIL()
+            .private('subscribed.successfully')
+            .listen('SubscribedSuccessfullyEvent', (response) => {
+                console.log(response);
+            });
+
+        return () => {
+            if (! AUTH.isAuthenticated) {
+                console.log('Leaving the Subscribed Successfully Event channel');
+                ECHO_UTIL().leaveChannel('SubscribedSuccessfullyEvent');
+            }
+        }
+    }, [AUTH.isAuthenticated]);
 
     return (
         <Container maxWidth='xl' className={ classes.container }>
@@ -24,4 +43,8 @@ const UserLayout = ({ children }) =>
     )
 }
 
-export default UserLayout
+const mapStateToProps = createStructuredSelector({
+    AUTH: selectAuth
+});
+
+export default connect(mapStateToProps)(UserLayout)
