2b20fef735bf897a921869e155a48f182a32d739
gene222-arch <genephillip222@gmail.com>
2021-11-01 09:00:22 +0800

    feat: dispatch update subscription details on subscribed successfully event

--- src/views/layouts/UserLayout.jsx
+++ src/views/layouts/UserLayout.jsx
@@ -4,6 +4,8 @@ import AppHeader from '../../components/app/AppHeader'
 import { makeStyles } from '@material-ui/core/styles'
 import Colors from './../../constants/Colors';
 import ECHO_UTIL from './../../utils/echo'
+import { useDispatch } from 'react-redux';
+import * as AUTH_ACTION from './../../redux/modules/auth/actions'
 
 const userLayoutUseStyles = makeStyles(theme => ({
     container: {
@@ -16,12 +18,13 @@ const userLayoutUseStyles = makeStyles(theme => ({
 const UserLayout = ({ children }) => 
 {
     const classes = userLayoutUseStyles();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         ECHO_UTIL()
             .private('subscribed.successfully')
-            .listen('SubscribedSuccessfullyEvent', (response) => {
-                console.log(response);
+            .listen('SubscribedSuccessfullyEvent', ({ data }) => {
+                dispatch(AUTH_ACTION.updateSubscriptionDetails({ subscription_details: data }));
             });
     }, []);
 
