73f8c11e70b0cbfd369843d7418b3d4a25b55f46
gene222-arch <genephillip222@gmail.com>
2021-11-02 10:30:54 +0800

    feat: update findPaymentAuthorizationsByUserIdAsync

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,9 +1,9 @@
 import axiosInstance from './../utils/axiosInstance'
 
-export const findPaymentAuthorizationsByUserIdAsync = async (userId) => 
+export const findPaymentAuthorizationsByUserIdAsync = async () => 
 {
     return await axiosInstance()
-        .get(`/notifications/payment-authorizations?userId=${ userId }`)
+        .get(`/notifications/payment-authorizations`)
         .then(response => response.data)
         .catch(error => Promise.reject(error.response.data));
 }
