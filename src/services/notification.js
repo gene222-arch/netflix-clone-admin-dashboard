fd223a81fbebe6709c1c3bebc9746b278f1370fc
gene222-arch <genephillip222@gmail.com>
2021-11-03 20:42:59 +0800

    feat(services/): add mark all as read and clear payment auth notifs api

--- src/services/notification.js
+++ src/services/notification.js
@@ -15,3 +15,19 @@ export const fetchAllPaymentAuthorizationByUserIdAsync = async () =>
         .then(response => response.data)
         .catch(error => Promise.reject(error.response.data));
 }
+
+export const markAllPaymentAuthNotifsAsReadAsync = async () => 
+{
+    return await axiosInstance()
+        .put(`/notifications/payment-authorizations/mark-all-as-read`)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const clearAllPaymentAuthNotifsAsync = async () => 
+{
+    return await axiosInstance()
+        .delete(`/notifications/payment-authorizations/clear`)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
