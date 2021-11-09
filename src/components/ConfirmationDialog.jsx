0f376e39ce527eb7c7d9f864544558ca966bfe4b
gene222-arch <genephillip222@gmail.com>
2021-11-09 22:12:54 +0800

    style: improve UI

--- src/components/ConfirmationDialog.jsx
+++ src/components/ConfirmationDialog.jsx
@@ -10,10 +10,36 @@ import { selectConfirm } from './../redux/modules/confirm/selector';
 import * as CONFIRM_ACTION from './../redux/modules/confirm/actions';
 import { connect, useDispatch } from 'react-redux';
 import { selectAuth } from './../redux/modules/auth/selector';
+import { makeStyles } from '@material-ui/core';
+import Colors from './../constants/Colors';
 
+
+const confirmationDialogUseStyles = makeStyles(theme => ({
+    confirmBtn: {
+        backgroundColor: Colors.success,
+        color: Colors.white,
+        '&:hover': {
+            backgroundColor: Colors.white,
+            color: Colors.success,
+        }
+    },
+    cancelBtn: {
+        color: Colors.dark,
+        '&:hover': {
+            color: Colors.error
+        }
+    },
+    dialogContent: {
+        backgroundColor: Colors.white
+    },
+    descriptionText: {
+        color: Colors.dark
+    }
+}));
 
 const ConfirmationDialog = ({ CONFIRM, AUTH }) => 
 {
+    const classes = confirmationDialogUseStyles();
     const dispatch = useDispatch();
 
     const handleClose = () => {
@@ -34,14 +60,40 @@ const ConfirmationDialog = ({ CONFIRM, AUTH }) =>
                 aria-labelledby='alert-dialog-title'
                 aria-describedby='alert-dialog-description'
                 fullWidth
+                PaperProps={{
+                    style: {
+                      backgroundColor: Colors.white,
+                      color: Colors.dark,
+                      height: '30vh'
+                    },
+                }}
             >
-                <DialogTitle id='alert-dialog-title'>{ CONFIRM.mainHeader }</DialogTitle>
+                <DialogTitle id='alert-dialog-title'>
+                    <strong>{ CONFIRM.mainHeader }</strong>
+                </DialogTitle>
                 <DialogContent>
-                    <DialogContentText id='alert-dialog-description'>{ CONFIRM.subHeader }</DialogContentText>
+                    <DialogContentText 
+                        id='alert-dialog-description'
+                        className={ classes.descriptionText }
+                    >
+                        { CONFIRM.subHeader }
+                    </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={ handleClose } disabled={ AUTH.isLoading }>{ CONFIRM.cancelText }</Button>
-                    <Button onClick={ handleClickConfirm } autoFocus disabled={ AUTH.isLoading }>
+                    <Button 
+                        onClick={ handleClose } 
+                        disabled={ AUTH.isLoading }
+                        className={ classes.cancelBtn }
+                    >
+                        { CONFIRM.cancelText }
+                    </Button>
+                    <Button 
+                        onClick={ handleClickConfirm } 
+                        variant='contained' 
+                        autoFocus 
+                        disabled={ AUTH.isLoading }
+                        className={ classes.confirmBtn }
+                    >
                         { !AUTH.isLoading ? CONFIRM.confirmText : 'Processing...' }
                     </Button>
                 </DialogActions>
