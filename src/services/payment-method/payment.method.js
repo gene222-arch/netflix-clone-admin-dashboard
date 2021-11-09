629ab0d82317b5477c3108e4b145d652bf273c85
gene222-arch <genephillip222@gmail.com>
2021-11-09 11:54:55 +0800

    feat(services/): update payment method

--- src/services/payment-method/payment.method.js
+++ src/services/payment-method/payment.method.js
@@ -7,3 +7,35 @@ export const ePaymentAsync = async (payload) =>
         .then(response => response.data)
         .catch(error => Promise.reject(error.response.data));
 }
+
+export const findPaymentIntentAsync = async (id) => 
+{
+    return await axiosInstance()
+        .get(`/payment-methods/payment-intents/${ id }`)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const storePaymentIntentAsync = async (payload) => 
+{
+    return await axiosInstance()
+        .post('/payment-methods/payment-intents', payload)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const attachPaymentMethodToIntentAsync = async (payload) => 
+{
+    return await axiosInstance()
+        .post('/payment-methods/payment-intents/attach-payment-method', payload)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export const cancelPaymentIntentAsync = async (id) => 
+{
+    return await axiosInstance()
+        .put(`/payment-methods/payment-intents/${ id }/cancel`)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data));
+}
