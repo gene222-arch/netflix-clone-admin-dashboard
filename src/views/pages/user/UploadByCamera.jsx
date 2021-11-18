c2c03ee5febbcc03f08274c9eb887c17dd9941b0
gene222-arch <genephillip222@gmail.com>
2021-11-18 09:30:57 +0800

    feat: add webcam setting up indicator

--- src/views/pages/user/UploadByCamera.jsx
+++ src/views/pages/user/UploadByCamera.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useEffect } from 'react'
+import React, { useCallback, useState, useRef, useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
@@ -8,6 +8,10 @@ import Colors from './../../../constants/Colors';
 import Webcam from 'react-webcam'
 import { uploadAvatarAsync } from './../../../services/auth/upload.avatar';
 import * as FILE_UTIL from './../../../utils/file'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import PhotoCamera from '@material-ui/icons/PhotoCamera'
+
+
 
 const videoConstraints = {
     width: 1280,
@@ -41,8 +45,12 @@ const uploadByCameraUseStyles = makeStyles(theme => ({
         height: '100%'
     },
     webcamContainer: {
-        textAlign: 'center'
-    },
+        textAlign: 'center',
+        width: '100%',
+        height: '18.75rem',
+        display: 'flex',
+        alignItems: 'center'
+    }
 }));
 
 const UploadByCamera = ({ handleUpload }) => 
@@ -50,22 +58,34 @@ const UploadByCamera = ({ handleUpload }) =>
     const classes = uploadByCameraUseStyles();
     const webcamRef = useRef(null);
 
+    const [ isCapturing, setIsCapturing ] = useState(false);
+    const [ isWebcamReady, setIsWebcamReady ] = useState(false);
+
     const handleClickCapture = useCallback(
         async () => 
         {
-            const imageSrc = webcamRef.current.getScreenshot();
-            const file = FILE_UTIL.base64ToFile(imageSrc, 'avatar-cam.jpg');
+            setIsCapturing(true);
+            const imageSrc = webcamRef.current.getScreenshot({ width: 300, height: 300 });
+            const file = FILE_UTIL.base64ToFile(imageSrc, 'avatar-webcam.jpg');
 
             const { data } = await uploadAvatarAsync({ avatar: file });
 
+            setIsCapturing(false);
             handleUpload(data);
         },
         [ webcamRef ]
     );
 
-    useEffect(() => {
+    useEffect(() => 
+    {
+        setTimeout(() => {
+            setIsWebcamReady(true);
+        }, 2500)
+
         return () => {
             webcamRef.current = null;
+            setIsCapturing(false);
+            setIsWebcamReady(false);
         }
     }, []);
     
@@ -77,13 +97,31 @@ const UploadByCamera = ({ handleUpload }) =>
             <Grid container spacing={ 3 } justify='center'>
                 <Grid item xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
                     <div className={ classes.webcamContainer }>
-                        <Webcam
-                            audio={ false }
-                            ref={ webcamRef }
-                            screenshotFormat="image/jpeg"
-                            videoConstraints={ videoConstraints }
-                            className={ classes.webcam }
-                        />
+                        
+                        {
+                            !isWebcamReady
+                                ? (
+                                    <Grid container spacing={1} alignItems='center'>
+                                        <Grid item xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
+                                            <PhotoCamera />
+                                        </Grid>
+                                        <Grid item xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
+                                            <Typography variant="subtitle1" color="initial" align="center">
+                                                Webcam starting...
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                )
+                                : (
+                                    <Webcam
+                                        audio={ false }
+                                        ref={ webcamRef }
+                                        screenshotFormat="image/jpeg"
+                                        videoConstraints={ videoConstraints }
+                                        className={ classes.webcam }
+                                    />
+                                )
+                        }
                     </div>
                 </Grid>
                 <Grid item xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
@@ -93,8 +131,11 @@ const UploadByCamera = ({ handleUpload }) =>
                         onClick={ handleClickCapture } 
                         fullWidth
                         className={ classes.btn }
+                        disabled={ isCapturing }
                     >
-                        Capture
+                        {
+                            isCapturing ? <CircularProgress /> : 'Capture'
+                        }
                     </Button>
                 </Grid>
             </Grid>
