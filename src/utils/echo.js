b414f60dacdc0b5175063e2973428016426200da
gene222-arch <genephillip222@gmail.com>
2021-11-30 03:05:49 +0800

    fix: wss error connection

--- src/utils/echo.js
+++ src/utils/echo.js
@@ -1,29 +1,32 @@
+/** Libraries */
 import Echo from "laravel-echo"
+
+/** Utils */
 import * as Cookies from './cookies'
 
 window.Pusher = require('pusher-js');
 
 export default () => 
 {
-	const authOptions = {
-		headers: {
-			Authorization: `Bearer ${ Cookies.get('access_token') }`,
-			Accept: 'application/json',
-		},
-	};
+    const auth = {
+        headers: {
+            Authorization: `Bearer ${ Cookies.get('access_token') }`,
+            Accept: 'application/json',
+        },
+    };
 
-    return new Echo(
-	{
+    const options = {
         broadcaster: process.env.REACT_APP_BROADCASTER,
         key: process.env.REACT_APP_PUSHER_APP_KEY,
-        auth: authOptions,
-        authEndpoint: process.env.REACT_APP_LARAVEL_API_BROADCAST_URL,
-        encrypted: process.env.REACT_APP_PUSHER_ENCRYPTED,
+        auth,
+        authEndpoint: process.env.REACT_APP_API_BROADCAST_BASE_URL,
+        encrypted: true,
         enableStats : false,
         enabledTransports: ['ws', 'wss'],
-        forceTLS: process.env.REACT_APP_PUSHER_FORCE_TLS,
+        forceTLS: false,
         wsHost: window.location.hostname,
-        wsPort: process.env.REACT_APP_PUSHER_WS_PORT,
-        wssPort: process.env.REACT_APP_PUSHER_WSS_PORT,
-    });
+        wsPort: process.env.REACT_APP_WS_PORT,
+    };
+
+    return new Echo(options);
 }
