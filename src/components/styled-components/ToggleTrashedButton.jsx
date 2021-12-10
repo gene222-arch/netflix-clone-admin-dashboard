09b1f7031c96d1057b48147aa394b5c45a6de168
gene222-arch <genephillip222@gmail.com>
2021-12-11 04:03:35 +0800

    feat: add ToggleTrashedButton component

---
+++ src/components/styled-components/ToggleTrashedButton.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
+import { FormControlLabel } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+const toggleTrashedButtonUseStyles = makeStyles(theme => ({
+    toggleTrashedContainer: {
+        textAlign: 'right',
+        padding: '1rem'
+    }
+}));
+
+
+const ToggleTrashedButton = ({ title = 'Show deleted items', onClick }) => 
+{
+    const classes = toggleTrashedButtonUseStyles();
+
+    const [ trashedOnly, setTrashedOnly ] = useState(false);
+
+    useEffect(() => {
+        return () => {
+            setTrashedOnly(false);
+        }
+    }, []);
+
+    return (
+        <div className={ classes.toggleTrashedContainer }>
+            <Tooltip title='Show deleted items'>
+                <FormControlLabel 
+                    control={
+                        <Switch 
+                            checked={ trashedOnly }
+                            onChange={ onClick }
+                        />
+                    } 
+                    label={ 
+                        <DeleteIcon 
+                            color={ `${ trashedOnly ? 'error' : 'disabled' }` }
+                        /> 
+                    } 
+                />
+            </Tooltip>
+        </div>
+    )
+}
+
+export default ToggleTrashedButton
