a1ccbfa27ecd93f748c46c23c783b89d81b30b5a
gene222-arch <genephillip222@gmail.com>
2021-12-11 02:40:25 +0800

    fix: restore movies auto dispatch

--- src/redux/modules/movie/saga.js
+++ src/redux/modules/movie/saga.js
@@ -178,7 +178,6 @@ export default function*()
         createMovieWatcher(),
         updateMovieWatcher(),
         restoreMoviesWatcher(),
-        restoreMoviesSaga(),
         deleteMoviesWatcher()
     ]);
 }
